ebf882768766cd6132d16304cc9599235132d149
yaamai <ya_amai@yahoo.co.jp>
2020-09-12 15:38:35 +0000

    Fix base image name

--- harbor/run.sh
+++ harbor/run.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-cd src
-make compile
-make build_base_docker
-make build
+which docker
+cp docker /usr/local/bin
+which docker
 
-docker images
+cd src
+
+for stg in compile build_base_docker build; do
+  make $stg \
+    -e VERSIONTAG=dev-amd64 \
+    -e BASEIMAGETAG=dev-amd64 \
+    -e PKGVERSIONTAG=dev-amd64 \
+    -e CHARTFLAG=true \
+    -e BUILDBIN=true \
+    -e TRIVYFLAG=true
+  docker images
+
+  make $stg \
+    -e VERSIONTAG=dev-arm64 \
+    -e BASEIMAGETAG=dev-arm64 \
+    -e PKGVERSIONTAG=dev-arm64 \
+    -e CHARTFLAG=true \
+    -e BUILDBIN=true \
+    -e TRIVYFLAG=true \
+    -e TRIVY_DOWNLOAD_URL=https://github.com/aquasecurity/trivy/releases/download/v0.11.0/trivy_0.11.0_Linux-ARM64.tar.gz 
+  docker images
+done
+
